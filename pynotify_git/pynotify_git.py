52dc60efa908ec3a087ffa783b8a8a7982f610da
AlexGhiti <alexandre@ghiti.fr>
2016-09-24 09:23:17 +0200

    pynotify_git: Create a new dev branch and add commit at each modification.

--- pynotify_git/pynotify_git.py
+++ pynotify_git/pynotify_git.py
@@ -4,59 +4,67 @@ import os
 import re
 import pyinotify
 import argparse
+import datetime
 from git import Git, Repo, Head, exc
 
 path_filter = [ 
-	".*\.git*", 		# git repo
-	".*\.swp*", 		# vim swap files
-	".*4913$", ".*5036$",	# vim tmp file that makes sure the directory
-	".*5159$", ".*5282$",	# is writable for swap files.
-] 
+    ".*\.git*", 		# git repo
+    ".*\.swp*", 		# vim swap files
+    ".*4913$", ".*5036$",	# vim tmp file that makes sure the directory
+    ".*5159$", ".*5282$",	# is writable for swap files.
+]
 
 class EventHandler(pyinotify.ProcessEvent):
-	def process_IN_CREATE(self, event):
-		for filter in path_filter:
-			if (re.match(filter, event.pathname)):
-				return
+  def process_IN_CREATE(self, event):
+    for filter in path_filter:
+      if (re.match(filter, event.pathname)):
+        return
 
-		print("File created %s !" % event.pathname)
-
-
-	def process_IN_MODIFY(self, event):
-		for filter in path_filter:
-			if (re.match(filter, event.pathname)):
-				return
-
-		print("File modified %s !" % event.pathname)
+    print("File created %s !" % event.pathname)
 
 
-def init_inotify():
-	wm = pyinotify.WatchManager()
-	mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY
+  def process_IN_MODIFY(self, event):
+    for filter in path_filter:
+      if (re.match(filter, event.pathname)):
+        return
 
-	handler = EventHandler()
-	notifier = pyinotify.Notifier(wm, handler)
-	wdd = wm.add_watch(args.dir, mask, rec = True)
-	notifier.loop()
+    print("File modified %s !" % event.pathname)
+    repo.index.add([event.pathname]) 
+    repo.index.commit("save %s" % event.pathname)
 
 
-def init_git():
-	# Create Repository object.
-	try:
-		repo = Repo(args.dir)
-	except exc.InvalidGitRepositoryError as e:
-		print("dir is not versioned.\n")
-		exit(0)
-	except exc.NoSuchPathError as e:
-		print("dir does not exist.\n")
-		exit(0)
-	
+def __format_time():
+  return datetime.datetime.now().strftime('%d%m%Y')
+
+
+def launch_inotify():
+  wm = pyinotify.WatchManager()
+  mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY
+
+  handler = EventHandler()
+  notifier = pyinotify.Notifier(wm, handler)
+  wdd = wm.add_watch(args.dir, mask, rec = True)
+  notifier.loop()
+
 
 parser = argparse.ArgumentParser(description = 'Automatically create git commit when a file is saved')
 parser.add_argument('--dir', default = "",
-	            help = 'Directory to watch.')
+                    help = 'Directory to watch.')
 args = parser.parse_args()
 
-init_git()
-init_inotify()
+try:
+  # Create Repository object.
+  repo = Repo(args.dir)
+  # Create a new head (<=> branch) for today's dev.
+  dev_branch = repo.create_head("dev/%s" % __format_time())
+  # And work on this new branch.
+  dev_branch.checkout()
+except exc.InvalidGitRepositoryError as e:
+  print("dir is not versioned.\n")
+  exit(0)
+except exc.NoSuchPathError as e:
+  print("dir does not exist.\n")
+  exit(0)
+
+launch_inotify()
 
